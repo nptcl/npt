de4e559f61121782280dff8b4abf1a3daf0c48aa
nptlisp <nptlisp@gmail.com>
2019-07-06 11:30:02 +0900

    add script file.

---
+++ clean_autoconf.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+checkerr()
+{
+	if [ $? -ne 0 ]; then
+		echo $*
+		exit 1
+	fi
+}
+
+if [ -f Makefile ]; then
+	make clean
+	checkerr "make clean error";
+fi
+
+rm -f Makefile
+checkerr "rm Makefile error"
+
+rm -f Makefile.in
+rm -f aclocal.m4
+rm -fr autom4te.cache/
+rm -f compile
+rm -f config.guess
+rm -f config.h.in
+rm -f config.sub
+rm -f configure
+rm -f depcomp
+rm -f install-sh
+rm -f missing
+rm -f config.h f config.log f config.status
+rm -fr src/.deps/
+rm -f src/.dirstamp
+rm -f stamp-h1
+
+echo OK
+
