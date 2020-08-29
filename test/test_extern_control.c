f746cbfdda24984c155add062252cb6139ac78b7
nptlisp <nptlisp@gmail.com>
2020-08-29 17:11:22 +0900

    Update testcase.

--- test/test_extern_control.c
+++ test/test_extern_control.c
@@ -1,12 +1,15 @@
 #include "extern_control.c"
 #include "character.h"
 #include "clos.h"
+#include "code.h"
 #include "common.h"
 #include "constant.h"
 #include "control.h"
+#include "declare.h"
 #include "degrade.h"
-#include "extern_sequence.h"
+#include "extern_execute.h"
 #include "extern_object.h"
+#include "extern_sequence.h"
 #include "extern_type.h"
 #include "integer.h"
 #include "object.h"
@@ -358,6 +361,154 @@ static int test_lisp_set_special32(void)
 
 
 /*
+ *  defvar
+ */
+static int test_lisp_defvar(void)
+{
+	addr control, x, y;
+
+	lisp_push_control(&control);
+	x = Lisp_hold();
+
+	lisp_intern8_(x, NULL, "LISP-DEFVAR-1");
+	hold_value(x, &y);
+	test(! specialp_symbol(y), "lisp_defvar.1");
+	lisp_defvar_(y);
+	test(specialp_symbol(y), "lisp_defvar.2");
+
+	lisp_intern8_(x, NULL, "LISP-DEFVAR-2");
+	hold_value(x, &y);
+	test(! specialp_symbol(y), "lisp_defvar.3");
+	lisp_defvar_(x);
+	test(specialp_symbol(y), "lisp_defvar.4");
+
+	lisp_pop_control_(control);
+
+	RETURN;
+}
+
+static int test_lisp_defvar8(void)
+{
+	addr control, x, y;
+
+	lisp_push_control(&control);
+	x = Lisp_hold();
+
+	lisp_intern8_(x, NULL, "LISP-DEFVAR8");
+	hold_value(x, &y);
+	test(! specialp_symbol(y), "lisp_defvar8.1");
+	lisp_defvar8_("LISP-DEFVAR8");
+	test(specialp_symbol(y), "lisp_defvar8.2");
+
+	lisp_pop_control_(control);
+
+	RETURN;
+}
+
+static int test_lisp_defvar16(void)
+{
+	addr control, x, y;
+	const byte16 str[] = { 'L','I','S','P','-','D','E','F','V','A','R','1','6',0 };
+
+	lisp_push_control(&control);
+	x = Lisp_hold();
+
+	lisp_intern8_(x, NULL, "LISP-DEFVAR16");
+	hold_value(x, &y);
+	test(! specialp_symbol(y), "lisp_defvar16.1");
+	lisp_defvar16_(str);
+	test(specialp_symbol(y), "lisp_defvar16.2");
+
+	lisp_pop_control_(control);
+
+	RETURN;
+}
+
+static int test_lisp_defvar32(void)
+{
+	addr control, x, y;
+	const unicode str[] = { 'L','I','S','P','-','D','E','F','V','A','R','3','2',0 };
+
+	lisp_push_control(&control);
+	x = Lisp_hold();
+
+	lisp_intern8_(x, NULL, "LISP-DEFVAR32");
+	hold_value(x, &y);
+	test(! specialp_symbol(y), "lisp_defvar32.1");
+	lisp_defvar32_(str);
+	test(specialp_symbol(y), "lisp_defvar32.2");
+
+	lisp_pop_control_(control);
+
+	RETURN;
+}
+
+
+/*
+ *  unwind-protect
+ */
+static int test_lisp_unwind_protect(void)
+{
+	addr a, b, x, y;
+
+	lisp_push_control(&a);
+	x = Lisp_hold();
+
+	lisp_fixnum(x, 100);
+	lisp_defvar8_("UNWIND-PROTECT-VALUE");
+	lisp_set_special8_("UNWIND-PROTECT-VALUE", x);
+
+	lisp_push_control(&b);
+	lisp_eval8_(x, "(lambda () (setq unwind-protect-value 200))");
+	lisp_unwind_protect(x);
+
+	lisp_pop_control_(b);
+	lisp_get_special8_(x, "UNWIND-PROTECT-VALUE");
+	test(lisp_fixnum_p(x), "lisp_unwind_protect.1");
+	lisp_hold_value(x, &y);
+	test(RefFixnum(y) == 200, "lisp_unwind_protect.2");
+
+	lisp_pop_control_(a);
+
+	RETURN;
+}
+
+
+/*
+ *  catch / throw
+ */
+static int test_lisp_catch(void)
+{
+	addr a, b, x;
+	Execute ptr;
+
+	ptr = Execute_Thread;
+	lisp_push_control(&a);
+	x = Lisp_hold();
+	lisp_intern8_(x, NULL, "HELLO");
+	lisp_catch(x);
+
+	lisp_push_control(&b);
+
+	test(! lisp_break_control(), "lisp_catch.1");
+	test(lisp_throw_(x) != 0, "lisp_catch.2");
+	test(! lisp_break_control(), "lisp_catch.3");
+	test(ptr->throw_value == throw_catch, "lisp_catch.4");
+
+	test(lisp_pop_control_(b) != 0, "lisp_catch.5");
+	test(lisp_break_control(), "lisp_catch.6");
+	lisp_reset_control();
+	test(! lisp_break_control(), "lisp_catch.7");
+	test(! lisp_escape_control(), "lisp_catch.8");
+	test(ptr->throw_value == throw_normal, "lisp_catch.9");
+
+	lisp_pop_control_(a);
+
+	RETURN;
+}
+
+
+/*
  *  Main
  */
 static int testcase_extern_control(void)
@@ -377,6 +528,15 @@ static int testcase_extern_control(void)
 	TestBreak(test_lisp_set_special8);
 	TestBreak(test_lisp_set_special16);
 	TestBreak(test_lisp_set_special32);
+	/* defvar */
+	TestBreak(test_lisp_defvar);
+	TestBreak(test_lisp_defvar8);
+	TestBreak(test_lisp_defvar16);
+	TestBreak(test_lisp_defvar32);
+	/* unwind-protect */
+	TestBreak(test_lisp_unwind_protect);
+	/* catch / throw */
+	TestBreak(test_lisp_catch);
 
 	return 0;
 }
@@ -397,6 +557,8 @@ static void testinit_extern_control(Execute ptr)
 	build_common();
 	build_reader();
 	build_pathname();
+	build_declare();
+	build_code();
 }
 
 int test_extern_control(void)
